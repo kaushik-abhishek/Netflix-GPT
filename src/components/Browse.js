41451579a5444646b46a9acfa7d2d3e394e1986b
kaushik-abhishek <vyasdusani@gmail.com>
2024-03-22 18:14:47 +0530

    created Movies Slice

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,16 +1,23 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constant";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
-  const getNowPlayingMovies = async() =>  {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+  const dispatch = useDispatch();
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS
+    );
     const json = await data?.json();
     console.log(json?.results);
-  }
+    dispatch(addNowPlayingMovies(json?.results));
+  };
   useEffect(() => {
     getNowPlayingMovies();
-  }, [])
+  }, []);
   return (
     <div>
       <Header />
