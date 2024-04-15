ddaca821ca3889268bc9131b1d7e6246aa286d93
kaushik-abhishek <vyasdusani@gmail.com>
2024-04-15 11:51:49 +0530

    TMDB API Fetch and built MainContainer

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,26 +1,15 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constant";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data?.json();
-    console.log(json?.results);
-    dispatch(addNowPlayingMovies(json?.results));
-  };
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -47,7 +47,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black flex justify-between z-10">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
         alt="logo"
